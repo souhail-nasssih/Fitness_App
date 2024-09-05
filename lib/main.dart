502e2ede129f2a6ed6b099a970cbb1c2f9d419db
souhail NASSIH <souhailnassih2@gmail.com>
2024-09-05 12:59:43 +0100

    commit6

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // 
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
     );
